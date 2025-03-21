972404c86857dff2abeebf1c75fab0aa2e4b4230
MarkussKevinsKirilko <144336415+MarkussKevinsKirilko@users.noreply.github.com>
2025-03-21 08:25:53 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,34 @@
 class Game:
-    def __init__(self):
-        return 0
+    def __init__(self, length, human_starts=True):
+        self.sequence = generate_sequence(length)
+        self.human_score = 50
+        self.comp_score = 50
+        self.is_comp_turn = not human_starts
+
+    def make_move(self, index, is_human):
+        number = self.sequence.pop(index)
+        if number == 1:
+            if is_human:
+                self.human_score -= 1
+            else:
+                self.comp_score -= 1
+        elif number == 2:
+            self.human_score -= 1
+            self.comp_score -= 1
+        else:
+            if is_human:
+                self.comp_score -= 1
+            else:
+                self.human_score -= 1
+        self.is_comp_turn = not is_human
+
+    def is_game_over(self):
+        return len(self.sequence) == 0
+
+    def get_winner(self):
+        if self.human_score > self.comp_score:
+            return "Cilvēks uzvar!"
+        elif self.comp_score > self.human_score:
+            return "Dators uzvar!"
+        else:
+            return "Neizšķirts."
